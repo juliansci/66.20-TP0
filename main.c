4b97bf7909a1e3db7f97179a58d7ae2b8204b9c6
jscialabba <jscialabba@localhost.localdomain>
2015-03-25 16:52:05 -0300

    Leo lineas mediante un buffer y no caracter por caracter.

--- main.c
+++ main.c
@@ -9,109 +9,94 @@ int isEndOfLine(char c) {
     return 0;
 }
 
-int isInResizeLine(int counterCharacterLine, int characterLineSize) {
-    if (counterCharacterLine + 1 >= characterLineSize) {
-        return 1;
+void concatBuffer(char** line, const char* buffer) {
+    size_t len1 = *line ? strlen(*line) : 0;
+    size_t len2 = buffer ? strlen(buffer) : 0;
+    char *res = realloc(*line, len1 + len2 + 1);
+    if (res) {
+        memcpy(res + len1, buffer, len2 + 1);
+        *line = res;
     }
-    return 0;
-}
-
-int isInResizeLines(int counterLines, int allLinesSize) {
-    if (counterLines + 1 >= allLinesSize) {
-        return 1;
-    }
-    return 0;
 }
 
 int tacFile(char* fileName, char* outputFileName) {
-	FILE *fp;
-	fp = fopen(fileName, "r");
-	if (fp == NULL) {
-		return (EXIT_FAILURE);
-	}
+    FILE *fp;
+    fp = fopen(fileName, "r");
+    if (fp == NULL) {
+        return (EXIT_FAILURE);
+    }
 
-	int allLinesSize = 2;
-	char** arrayLines = (char**) malloc(sizeof (char*) * allLinesSize);
-	int counterLines = 0;
-	int initCharacterLineSize = 10;
-	int characterLineSize = 10;
-	char* line = malloc(initCharacterLineSize);
-	char character;
-	int counterCharacterLine = 0;
+    FILE *resultFile;
+    resultFile = fopen(outputFileName, "w");
 
-	while ((character = fgetc(fp)) != EOF) {
-		line[counterCharacterLine] = character;
-		counterCharacterLine++;
-		if (isInResizeLine(counterCharacterLine, characterLineSize)) {
-			characterLineSize *= 2;
-			line = realloc(line, characterLineSize);
-		}
-		if (isEndOfLine(character)) {
-			if (isInResizeLines(counterLines, allLinesSize)) {
-				allLinesSize *= 2;
-				arrayLines[allLinesSize];
-				arrayLines = (char**) realloc(arrayLines, sizeof (char*) * allLinesSize);
-			}
-			arrayLines[counterLines] = line;
-			characterLineSize = initCharacterLineSize;
-			line = malloc(characterLineSize);
-			counterLines++;
-			counterCharacterLine = 0;
-		}
-	}
-	counterLines--;
+    if (resultFile == NULL) {
+        printf("Error opening file: %s!\n", outputFileName);
+        exit(1);
+    }
 
-	FILE *resultFile;
-	resultFile = fopen(outputFileName, "w");
+    char ** info = NULL;
+    int lineLenght;
+    int lineCounter = 0;
+    const int bufIncrSize = 11;
+    char buffer[bufIncrSize];
+    char* line = (char *) calloc(bufIncrSize, sizeof (char));
+    while (fgets(buffer, bufIncrSize, fp)) {
+        concatBuffer(&line, buffer);
+        char lastCharacterBuffer = buffer[strlen(buffer) - 1];
+        if (isEndOfLine(lastCharacterBuffer)) {
+            info = realloc(info, (lineCounter + 1) * sizeof (char *));
+            lineLenght = strlen(line);
+            info[lineCounter] = calloc(sizeof (char), lineLenght + 1);
+            strcpy(info[lineCounter], line);
+            lineCounter++;
+            line = (char *) calloc(bufIncrSize, sizeof (char));
+        }
+    }
 
-	if (resultFile == NULL) {
-	    printf("Error opening file: %s!\n",outputFileName);
-	    exit(1);
-	}
+    int i;
+    for (i = lineCounter - 1; i >= 0; i--) {
+        fprintf(resultFile, "%s", info[i]);
+    }
 
-	for (counterLines; counterLines >= 0; counterLines--) {
-//		printf("%s", arrayLines[counterLines]);
-		fprintf(resultFile, "%s", arrayLines[counterLines]);
-	}
+    fclose(fp);
+    fclose(resultFile);
 
-	fclose(fp);
-	fclose(resultFile);
-	return (EXIT_SUCCESS);
+    return (EXIT_SUCCESS);
 }
 
 int main(int argc, char** argv) {
     if (argc < 2) {
-    	printf("%s","Faltan parametros\n");
+        printf("%s", "Faltan parametros\n");
         return (EXIT_FAILURE);
     }
 
     if ((strcmp(argv[1], "-h") == 0) || (strcmp(argv[1], "--help") == 0)) {
-		printf("%s", "Usage:\n");
-		printf("%s", "	tp0 -h\n");
-		printf("%s", "	tp0 -V\n");
-		printf("%s", "	tp0 [file...]\n");
-		printf("%s", "Options:\n");
-		printf("%s", "	-V, --version Print version and quit.\n");
-		printf("%s", "	-h, --help Print this information and quit.\n");
-		printf("%s", "Examples:\n");
-		printf("%s", "	tp0 foo.txt bar.txt\n");
-		printf("%s", "	tp0 gz.txt\n");
-		return (EXIT_SUCCESS);
+        printf("%s", "Usage:\n");
+        printf("%s", "	tp0 -h\n");
+        printf("%s", "	tp0 -V\n");
+        printf("%s", "	tp0 [file...]\n");
+        printf("%s", "Options:\n");
+        printf("%s", "	-V, --version Print version and quit.\n");
+        printf("%s", "	-h, --help Print this information and quit.\n");
+        printf("%s", "Examples:\n");
+        printf("%s", "	tp0 foo.txt bar.txt\n");
+        printf("%s", "	tp0 gz.txt\n");
+        return (EXIT_SUCCESS);
     }
     if ((strcmp(argv[1], "-V") == 0) || (strcmp(argv[1], "--version") == 0)) {
-		printf("%s", "Falta agregar información sobre la versión.\n");
-		return (EXIT_SUCCESS);
+        printf("%s", "Falta agregar información sobre la versión.\n");
+        return (EXIT_SUCCESS);
     }
 
     char* outputFileName;
 
     if (argc >= 3) {
-    	outputFileName = argv[2];
+        outputFileName = argv[2];
     } else {
-    	outputFileName = "output/resultFile.txt";
+        outputFileName = "output/resultFile.txt";
     }
 
-    int result = tacFile(argv[1],outputFileName);
+    int result = tacFile(argv[1], outputFileName);
     return (result);
 }
 
