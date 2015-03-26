71b71de3794a110b1158c5f7822aa931075b2bc4
jscialabba <jscialabba@localhost.localdomain>
2015-03-26 11:48:51 -0300

    Subo script que testea el programita.

---
+++ test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+testFile() {
+echo -e "\nTEST "$1".txt"
+./tac test-files/$1.txt output/$1-reverse.txt
+./tac output/$1-reverse.txt output/$1.txt
+res1=($(md5sum test-files/$1.txt))
+res2=($(md5sum output/$1.txt))
+if [ $res1 == $res2 ]; 
+then
+	echo -e "OK\n"
+else
+	echo -e "ERROR\n"
+fi
+}
+
+echo Compilando...
+gcc -o tac main.c
+
+testFile empty
+testFile basic
+testFile empty-lines
+testFile large-file
+
