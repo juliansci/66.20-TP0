2c6b7de4b105a491ff3a1047644402ea7157a86e
jscialabba <jscialabba@localhost.localdomain>
2015-03-30 23:19:57 -0300

    Libero memoria.

--- main.c
+++ main.c
@@ -3,100 +3,103 @@
 #include <string.h>
 
 void printHelpInfo() {
-	printf("%s", "Usage:\n");
-	printf("%s", "	tp0 -h\n");
-	printf("%s", "	tp0 -V\n");
-	printf("%s", "	tp0 [file...]\n");
-	printf("%s", "Options:\n");
-	printf("%s", "	-V, --version Print version and quit.\n");
-	printf("%s", "	-h, --help Print this information and quit.\n");
-	printf("%s", "Examples:\n");
-	printf("%s", "	tp0 foo.txt bar.txt\n");
-	printf("%s", "	tp0 gz.txt\n");
+    printf("%s", "Usage:\n");
+    printf("%s", "	tp0 -h\n");
+    printf("%s", "	tp0 -V\n");
+    printf("%s", "	tp0 [file...]\n");
+    printf("%s", "Options:\n");
+    printf("%s", "	-V, --version Print version and quit.\n");
+    printf("%s", "	-h, --help Print this information and quit.\n");
+    printf("%s", "Examples:\n");
+    printf("%s", "	tp0 foo.txt bar.txt\n");
+    printf("%s", "	tp0 gz.txt\n");
 }
 
 void printVersionInfo() {
-	printf("%s", "tp0 1.1\n");
-	printf("%s", "Copyright © 2015 FIUBA.\n");
-	printf("%s", "Esto es software libre: usted es libre de cambiarlo y redistribuirlo.\n");
-	printf("%s", "No hay NINGUNA GARANTÍA, hasta donde permite la ley.\n");
-	printf("%s", "\n");
-	printf("%s", "Escrito por Julian Scialabba, Kaoru Heanna y Paula Saffioti.\n");
+    printf("%s", "tp0 1.1\n");
+    printf("%s", "Copyright © 2015 FIUBA.\n");
+    printf("%s", "Esto es software libre: usted es libre de cambiarlo y redistribuirlo.\n");
+    printf("%s", "No hay NINGUNA GARANTÍA, hasta donde permite la ley.\n");
+    printf("%s", "\n");
+    printf("%s", "Escrito por Julian Scialabba, Kaoru Heanna y Paula Saffioti.\n");
 }
 
 int isEndOfLine(char c) {
-	if (c == '\n') {
-		return 1;
-	}
-	return 0;
+    if (c == '\n') {
+        return 1;
+    }
+    return 0;
 }
 
 void concatBuffer(char** line, const char* buffer) {
-	size_t len1 = *line ? strlen(*line) : 0;
-	size_t len2 = buffer ? strlen(buffer) : 0;
-	char* concat = realloc(*line, len1 + len2 + 1);
-	if (concat) {
-		memcpy(concat + len1, buffer, len2 + 1);
-		*line = concat;
-	}
+    size_t len1 = *line ? strlen(*line) : 0;
+    size_t len2 = buffer ? strlen(buffer) : 0;
+    char* concat = realloc(*line, len1 + len2 + 1);
+    if (concat) {
+        memcpy(concat + len1, buffer, len2 + 1);
+        *line = concat;
+    }
 }
 
 int tacFile(FILE* fp) {
-	const int bufIncrSize = 10;
-	char ** arrayLines = NULL;
-	int lineCounter = 0;
-	char buffer[bufIncrSize];
-	char* line = (char *) calloc(bufIncrSize, sizeof(char));
-	while (fgets(buffer, bufIncrSize, fp)) {
-		concatBuffer(&line, buffer);
-		char lastCharacterBuffer = buffer[strlen(buffer) - 1];
-		if (isEndOfLine(lastCharacterBuffer)) {
-			arrayLines = realloc(arrayLines,
-					(lineCounter + 1) * sizeof(char *));
-			arrayLines[lineCounter] =  line;
-			lineCounter++;
-			line = (char *) calloc(bufIncrSize, sizeof(char));
-		}
-	}
+    const int bufIncrSize = 10;
+    char ** arrayLines = NULL;
+    int lineCounter = 0;
+    char buffer[bufIncrSize];
+    char* line = (char *) calloc(bufIncrSize, sizeof (char));
+    while (fgets(buffer, bufIncrSize, fp)) {
+        concatBuffer(&line, buffer);
+        char lastCharacterBuffer = buffer[strlen(buffer) - 1];
+        if (isEndOfLine(lastCharacterBuffer)) {
+            arrayLines = realloc(arrayLines,
+                    (lineCounter + 1) * sizeof (char *));
+            arrayLines[lineCounter] = line;
+            lineCounter++;
+            line = (char *) calloc(bufIncrSize, sizeof (char));
+        }
+    }
 
-	int i;
-	for (i = lineCounter - 1; i >= 0; i--) {
-		printf("%s", arrayLines[i]);
-	}
+    int i;
+    for (i = lineCounter - 1; i >= 0; i--) {
+        printf("%s", arrayLines[i]);
+        free(arrayLines[i]);
 
-	return (EXIT_SUCCESS);
+    }
+    free(line);
+    free(arrayLines);
+    return (EXIT_SUCCESS);
 }
 
 int main(int argc, char** argv) {
 
-	if ((argc == 2) && ((strcmp(argv[1], "-h") == 0) || (strcmp(argv[1], "--help") == 0))) {
-		printHelpInfo();
-		return (EXIT_SUCCESS);
-	}
+    if ((argc == 2) && ((strcmp(argv[1], "-h") == 0) || (strcmp(argv[1], "--help") == 0))) {
+        printHelpInfo();
+        return (EXIT_SUCCESS);
+    }
 
-	if ((argc == 2) && ((strcmp(argv[1], "-V") == 0) || (strcmp(argv[1], "--version") == 0))) {
-		printVersionInfo();
-		return (EXIT_SUCCESS);
-	}
+    if ((argc == 2) && ((strcmp(argv[1], "-V") == 0) || (strcmp(argv[1], "--version") == 0))) {
+        printVersionInfo();
+        return (EXIT_SUCCESS);
+    }
 
-	int result;
+    int result;
 
-	if (argc < 2) { //no tengo archivo de entrada, uso standard input
-		result = tacFile(stdin);
-		return (result);
-	}
+    if (argc < 2) { //no tengo archivo de entrada, uso standard input
+        result = tacFile(stdin);
+        return (result);
+    }
 
-	int i;
-	for (i = 1; i < argc; i++){
-		FILE *fp;
-		fp = fopen(argv[i], "r");
-		if (fp == NULL) {
-			fprintf (stderr, "%s", argv[i]);
-			fprintf (stderr, ": nombre de archivo o comando inválido.\n");
-			return (EXIT_FAILURE);
-		}
-		int result = tacFile(fp);
-		fclose(fp);
-	}
-	return (EXIT_SUCCESS);
+    int i;
+    for (i = 1; i < argc; i++) {
+        FILE *fp;
+        fp = fopen(argv[i], "r");
+        if (fp == NULL) {
+            fprintf(stderr, "%s", argv[i]);
+            fprintf(stderr, ": nombre de archivo o comando inválido.\n");
+            return (EXIT_FAILURE);
+        }
+        tacFile(fp);
+        fclose(fp);
+    }
+    return (EXIT_SUCCESS);
 }
